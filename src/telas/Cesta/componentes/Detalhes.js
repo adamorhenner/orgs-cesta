f2e437c6561af7fdb69714f83b1849c9dd4e5571
Adamor Henner <adacandido9@gmail.com>
2023-02-07 09:26:21 -0300

    Botao de compra

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,9 +1,9 @@
 import Texto from '../../../componentes/Texto';
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, View, TouchableOpacity } from "react-native";
 import logo from '../../../../assets/logo.png'
 
 
-export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco }){
+export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }){
     return <>
         <Texto style={estilos.nome}>{ nome }</Texto>
         <View style={estilos.fazenda}>
@@ -14,6 +14,10 @@ export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, pr
             { descricao }
         </Texto>
         <Texto style={estilos.preco}>{ preco }</Texto>
+
+        <TouchableOpacity style={estilos.botao} onPress={() => {}}>
+            <Texto style={estilos.textoBotao}>{ botao }</Texto>
+        </TouchableOpacity>
     </>
 }
 
@@ -49,4 +53,17 @@ const estilos = StyleSheet.create({
         lineHeight: 42,
         marginTop:8,
     },
+    botao: {
+        marginTop: 16,
+        backgroundColor:'#2A9F85',
+        paddingVertical: 16,
+        borderRadius: 6,
+    },
+    textoBotao: {
+        textAlign: "center",
+        color: "#ffffff",
+        fontSize: 16,
+        lineHeight: 26,
+        fontWeight: "bold",
+    }
 });
