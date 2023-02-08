bfea314182031862c9ad98c9490681c7b56d9401
Adamor Henner <adacandido9@gmail.com>
2023-02-08 18:07:02 -0300

    componente de lista

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,4 +1,10 @@
 import logo from '../../assets/logo.png';
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abóbora.png';
+
 
 const cesta = {
     topo: {
@@ -11,6 +17,31 @@ const cesta = {
         descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
         preco: "R$ 40,00",
         botao: "Comprar"
+    },
+    itens:{
+        titulo: "Itens da Cesta",
+        lista: [
+            {
+                nome: "Tomate",
+                image: tomate,
+            },
+            {
+                nome: "Brócolis",
+                image: brocolis,
+            },
+            {
+                nome: "Batata",
+                image: batata,
+            },
+            {
+                nome: "Pepino",
+                image: pepino,
+            },
+            {
+                nome: "Abóbora",
+                image: abobora,
+            }
+        ]
     }
 }
 
